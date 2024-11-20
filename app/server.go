5df58abb4e9269f1238c7dd9eb83cb2bf794b303
Abdullah <abdullah.yousuf13@gmail.com>
2024-11-20 00:56:01 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -22,19 +22,25 @@ func main() {
 	os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	buf := make([]byte, 1024)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 10240)
 	for{
 		_, err :=conn.Read(buf)	
 		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
-			os.Exit(1)
+			fmt.Println("Failed to read")
+			fmt.Println(err)
 			}
 		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }

df90d4ebf3c149ff9d6a7d236250ac85cd7d4aa5
Abdullah <abdullah.yousuf13@gmail.com>
2024-11-20 00:31:36 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -28,9 +28,13 @@ func main() {
 		os.Exit(1)
 	}
 	buf := make([]byte, 1024)
-	_, err :=conn.Read(buf)
-	conn.Write([]byte("+PONG\r\n"))
-	_, err :=conn.Read(buf)
-	conn.Write([]byte("+PONG\r\n"))
+	for{
+		_, err :=conn.Read(buf)	
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+			}
+		conn.Write([]byte("+PONG\r\n"))
+	}
 
 }

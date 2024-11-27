f092ab33c1ab596c164181af55121747be1df2bb
bozosort <abdullah.yousuf13@gmail.com>
2024-11-27 03:07:08 +0000

    codecrafters submit [skip ci]

--- app/RESP_Parser/RESP_Parser.go
+++ app/RESP_Parser/RESP_Parser.go
@@ -40,7 +40,7 @@ func DeserializeRESP(reader *bufio.Reader) (*RESPValue, int, error) {
 		data := make([]byte, length)
 		reader.Read(data)
 		fmt.Println("data[:length]")
-		fmt.Println(data[:length])
+		fmt.Println(string("data[:length]"))
 		return &RESPValue{"BulkString", string(data[:length])}, length + len(line) + 1, nil
 	case '*': // Array
 		line, _ := reader.ReadString('\n')
